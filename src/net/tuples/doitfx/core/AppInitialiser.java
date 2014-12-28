64995e2a4f19bd0c479ab181add78c0d103e6467
Geuntaek Lee <ideatuples@outlook.com>
2014-12-28 13:13:52 +0900

    Adding javadoc contents for 2 files.

--- src/net/tuples/doitfx/core/AppInitialiser.java
+++ src/net/tuples/doitfx/core/AppInitialiser.java
@@ -1,8 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Geuntaek Lee
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package net.tuples.doitfx.core;
 
 import net.tuples.doitfx.connector.ConnectManager;
@@ -12,9 +25,18 @@ import net.tuples.doitfx.connector.crypto.PasswordManager;
 import net.tuples.doitfx.core.config.AppConfigManager;
 
 /**
- *
- * @author ideatuples
- */
+ * AppInitialiser is the initialiser of DoitFX.
+ * 
+ * AppInitialiser controls every manager.
+ * This is a kind of implementation of the Singleton design pattern.
+ * 
+ * It also has some methods to return each manager to the point of the calling.
+ * 
+ * 
+ * @author Geunatek Lee
+ * @version 0.0.1, 26 Dec 2014
+ **/
+
 public class AppInitialiser {
 
     private static AppInitialiser appInitialiser;
@@ -50,27 +72,68 @@ public class AppInitialiser {
         return true;
     }
 
+    /**
+     * For getting AccountConfigManager class.
+     * 
+     * To load the raw configuration Properties classes, you need to invoke
+     * this method first and find whatever you in configuration files.
+     * 
+     * @return AccountConfigManager
+     */
     public final AccountConfigManager getAccConfigManager() {
         return accConfigManager;
     }
 
+    /**
+     * For getting ConnectManager class.
+     * 
+     * If you go to get Store objects which hold something to receive e-mails 
+     * from servers, call this method and take it.
+     * 
+     * @return ConnectManager
+     */
     public final ConnectManager getConnectManager() {
         return connectManager;
     }
-
+    
+    
+    /**
+     * For getting PasswordManager class.
+     * 
+     * If you go to get Store objects which hold something to receive e-mails 
+     * from servers, call this method and take it.
+     * 
+     * @return PasswordManager
+     */
     public final PasswordManager getPWManager() {
         return pwManager;
     }
 
-    
+    /**
+     * For getting SessionManager class for receiving messages.
+     * 
+     * @return SessionManager
+     */
     public final SessionManager getRecvSessionManager() {
         return recvSessionManager;
     }
 
+    /**
+     * For getting SessionManager class for sending messages.
+     * 
+     * @return SessionManager
+     */
     public final SessionManager getSendSessionManager() {
         return sendSessionManager;
     }
     
+    /**
+     * getInstance is a famous type of methods.
+     * 
+     * You can get AppInitialiser at any time, at any where.
+     * 
+     * @return AppIntialiser itself. 
+     */
     public static AppInitialiser getInstance() {
         
         if(appConfigManager == null) {
--- src/net/tuples/doitfx/core/MessageReceiver.java
+++ src/net/tuples/doitfx/core/MessageReceiver.java
@@ -1,8 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Geuntaek Lee
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package net.tuples.doitfx.core;
 
 import java.util.ArrayList;
@@ -14,12 +27,23 @@ import javax.mail.Folder;
 import javax.mail.MessagingException;
 import javax.mail.Store;
 import net.tuples.doitfx.connector.ConnectManager;
-import net.tuples.doitfx.connector.Connector;
+
+
 
 /**
- *
- * @author ideatuples
- */
+ * MessageReceiver is a manipulator of message receiving procedure.
+ * 
+ * This is very similar to TV remote controller.
+ * It makes developers use DoitFX simply.
+ * 
+ * You can get MessageReceiver object from Connect object.
+ * You don't need to create this object directly.
+ * 
+ * @author Geunatek Lee
+ * @version 0.0.1, 26 Dec 2014
+ * 
+ **/
+
 public class MessageReceiver {
     private final AppInitialiser appInit;
     private final ConnectManager connectManager;
@@ -58,6 +82,12 @@ public class MessageReceiver {
         return null;
     }
     
+    /**
+     * Getting entire folder list of specific SvcPair.
+     * 
+     * @param pSvcPair The indicator of an e-mail account.
+     * @return List of Folder object.
+     */
     public final List<Folder> getEntireFolder(final String pSvcPair) {
         
         final Folder rootFolder;
@@ -70,12 +100,18 @@ public class MessageReceiver {
             return subFolders;
             
         } catch (MessagingException ex) {
-            Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MessageReceiver.class.getName()).log(Level.SEVERE, null, ex);
         }
        
         return null;
     }
     
+    /**
+    * Getting one folder of specific SvcPair you want.
+    * 
+    * @param pSvcPair The indicator of an e-mail account.
+    * @return Specific Folder object.
+    */
     public final Folder getFolder(final String pSvcPair, final String pFolderName) {
         
         final Folder rootFolder;
@@ -94,6 +130,12 @@ public class MessageReceiver {
         return null;
     }
     
+    /**
+    * Getting counts of Folder objects.
+    * 
+    * @param pSvcPair The indicator of an e-mail account.
+    * @return counts of Folder objects.
+    */
     public final int getFolderCounts(final String pSvcPair) {
         
         List<Folder> subFolders = getEntireFolder(pSvcPair);
@@ -101,6 +143,12 @@ public class MessageReceiver {
         return subFolders.size();
     }
     
+    /**
+     * Getting entire folder name String objects of specific SvcPair.
+     * 
+     * @param pSvcPair The indicator of an e-mail account.
+     * @return List of String object of folders.
+     */
     public final List<String> getFolderNames(final String pSvcPair) {
         
         final List<Folder> subFolders;
@@ -118,12 +166,34 @@ public class MessageReceiver {
         return subFolderNames;
     }
     
+    /**
+     * FolderManipulator is a manipulator of every Folder object.
+     * 
+     * If you go to create, update, delete a folder, you need to have this 
+     * manipulator object to do them.
+     * 
+     * You don't need to create this by your hands.
+     * This class is a set of static methods.
+     * 
+     * @author Geunatek Lee
+     * @version 0.0.1, 26 Dec 2014
+     * 
+     **/
     public static class FolderManipulator { 
         
         private FolderManipulator() {
             
         }
         
+        /**
+         * Checkout folder name which is already generated.
+         * 
+         * @param pRootFolder Root folder means the folder you get 
+         * with the method of MessageReceiver.getRootFolder()
+         * 
+         * @param pFolderName The folder name that you are looking for.
+         * @return True or False
+         */
         public static final boolean 
             checkExistingFolder(final Folder pRootFolder, final String pFolderName) {
             
@@ -148,6 +218,14 @@ public class MessageReceiver {
             return false;
         }
         
+        /**
+         * Generate new folder
+         * 
+         * @param pTgtStore 
+         * @param pFolderName The folder name that you want to name this folder.
+         * 
+         * @return True or False
+         */
         public static final boolean generate(final Store pTgtStore, final String pFolderName) {
             
             final Folder rootFolder;
@@ -174,6 +252,15 @@ public class MessageReceiver {
             return false;
         }
         
+        /**
+         * Delete a folder that has been existed on account.
+         * 
+         * @param pTgtStore
+         * @param pFolderName The name of the folder.
+         * @param pIsRecursive Check to get confirm to delete including
+         * sub-directories.
+         * @return True or False
+         */
         public static final boolean 
             delete(final Store pTgtStore, 
                     final String pFolderName, final boolean pIsRecursive) {
@@ -202,7 +289,15 @@ public class MessageReceiver {
 
             return false;
         }
-            
+        
+        /**
+         * Rename the name of a folder.
+         * 
+         * @param pTgtStore
+         * @param pSrcFolderName The original folder name
+         * @param pTgtFolderName The name you want to rename.
+         * @return True or False
+         */
         public static final boolean 
             rename(final Store pTgtStore, 
                     final String pSrcFolderName, final String pTgtFolderName) {
@@ -234,6 +329,7 @@ public class MessageReceiver {
             
             return false;
         }
+        
         
         public static final int getSubFolderCounts(final Store pTgtStore) {
 
